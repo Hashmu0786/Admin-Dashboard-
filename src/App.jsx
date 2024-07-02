2b638f8a1dcc0f133dc843d5c5525be6af6c825b
Hashmu0786 <hasmuddin97175@gmail.com>
2024-07-02 17:19:37 +0530

    add the page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+// import "./App.css";
+// import Layout from "./layout/Index.jsx";
+// import Dashboard from "./dashboard/index.jsx";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from "react-router-dom";
+// import Attendance from "./attendence/index.jsx";
+// import Employee from "./employee/index.jsx";
+// import Leave from "./leave/index.jsx";
+// import Report from "./report/index.jsx";
+// import Signin from "./signin/index.jsx";
+// import Cookies from "js-cookie";
+// import PageNotFound from "./sharedComponents/PageNotFound.jsx";
+
+// export default function App() {
+//   const isAuthenticated = Cookies.get("token");
+
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route exact path="/signin" element={<Signin />} />
+//         <Route
+//           path="/"
+//           element={!isAuthenticated ? <Navigate to="/signin" /> : <Layout />}
+//         >
+//           <Route index element={<Dashboard />} />
+//           <Route path="dashboard" element={<Dashboard />} />
+//           <Route path="attendance" element={<Attendance />} />
+//           <Route path="employee" element={<Employee />} />
+//           <Route path="leave" element={<Leave />} />
+//           <Route path="report" element={<Report />} />
+//         </Route>
+//       </Routes>
+//     </Router>
+
+//   );
+// }
+
 import "./App.css";
 import Layout from "./layout/Index.jsx";
 import Dashboard from "./dashboard/index.jsx";
@@ -13,6 +54,7 @@ import Leave from "./leave/index.jsx";
 import Report from "./report/index.jsx";
 import Signin from "./signin/index.jsx";
 import Cookies from "js-cookie";
+import PageNotFound from "./sharedComponents/PageNotFound.jsx";
 
 export default function App() {
   const isAuthenticated = Cookies.get("token");
@@ -20,7 +62,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/signin" element={<Signin />} />
+        <Route path="/signin" element={<Signin />} />
         <Route
           path="/"
           element={!isAuthenticated ? <Navigate to="/signin" /> : <Layout />}
@@ -31,7 +73,11 @@ export default function App() {
           <Route path="employee" element={<Employee />} />
           <Route path="leave" element={<Leave />} />
           <Route path="report" element={<Report />} />
+          {/* Catch-all route for Page Not Found */}
+          <Route path="*" element={<PageNotFound />} />
         </Route>
+        {/* Catch-all route for Page Not Found outside of authenticated routes */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
